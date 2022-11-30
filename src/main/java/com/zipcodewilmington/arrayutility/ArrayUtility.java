63a635b572f3588258ccae51f8e9ae08335f5e45
KashElcock <116089696+KashElcock@users.noreply.github.com>
2022-11-30 11:39:58 -0500

    format 12/16

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -5,10 +5,35 @@ import java.util.Arrays;
  * Created by leon on 3/6/18.
  */
 public class ArrayUtility <K>{K[] inputArray;
+
     public ArrayUtility(K[] inputArray){this.inputArray = inputArray;}
+
     ArrayList<K>mergedArray = new ArrayList<>();
-    public int countDuplicatesInMerge(K[] arrayToMerge, K valueToEvaluate) {int count = 0;mergedArray.addAll(Arrays.asList(inputArray));mergedArray.addAll((Arrays.asList(arrayToMerge)));for (K k : mergedArray) if (valueToEvaluate == k) count++;return count;}
+
+    public int countDuplicatesInMerge(K[] arrayToMerge, K valueToEvaluate) {
+        int count = 0;
+        mergedArray.addAll(Arrays.asList(inputArray));
+        mergedArray.addAll((Arrays.asList(arrayToMerge)));
+        for (K k : mergedArray) if (valueToEvaluate == k) count++;
+        return count;
+    }
+
     public K getMostCommonFromMerge(K[] arrayToMerge) {return null;}
-    public int getNumberOfOccurrences(K valueToEvaluate) {int count = 0;for(K val : inputArray ) if ((val == valueToEvaluate))count++;return count;}
-    public K[] removeValue(K valueToRemove) {ArrayList<K>list = new ArrayList<>(Arrays.asList(inputArray));for (int i = 0; i < list.size(); i++) {if(list.get(i)==valueToRemove){list.remove(i);i-=1;}}K[]array=Arrays.copyOf(inputArray,list.size());for (int i = 0; i < array.length; i++) array[i] = list.get(i);return array;}
+
+    public int getNumberOfOccurrences(K valueToEvaluate) {
+        int count = 0;
+        for(K val : inputArray ) if ((val == valueToEvaluate))count++;
+        return count;
+    }
+    public K[] removeValue(K valueToRemove) {
+        ArrayList<K>list = new ArrayList<>(Arrays.asList(inputArray));
+        for (int i = 0; i < list.size(); i++) {
+            if(list.get(i)==valueToRemove){
+                list.remove(i);
+                i-=1;
+            }
+        }
+        K[]array=Arrays.copyOf(inputArray,list.size());
+        for (int i = 0; i < array.length; i++) array[i] = list.get(i);
+        return array;}
 }
